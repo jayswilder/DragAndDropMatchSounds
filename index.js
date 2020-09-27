f564502dfd9aa55245d4f2a5d3d611f7fd5e069d
Jay Wilder <jayswilder@yahoo.com>
2020-09-26 20:17:30 -0500

    added audio to first batch of photos and commented out the remaining audio waiting for 45 more photos to add

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// goat
+
 const playGoatAudio = () => {
     const goatPronunciationAudio = new Audio('https://www.macmillandictionary.com/media/american/us_pron/g/goa/goat_/goat_American_English_pronunciation.mp3')
     goatPronunciationAudio.play()
@@ -7,6 +9,7 @@ const goatAudio = document.getElementById('goat');
 
 goatAudio.addEventListener('click', playGoatAudio)
 
+// cab
 const playCabAudion = () => {
     const cabAudio = new Audio('https://www.macmillandictionary.com/media/american/us_pron/c/cab/cab_A/cab_American_English_pronunciation.mp3');
     cabAudio.play()
@@ -16,6 +19,7 @@ const cabAudio = document.getElementById('cab');
 
 cabAudio.addEventListener('click', playCabAudion)
 
+// chair
 
 const playChairAudio = () => {
     const chairAudio = new Audio('https://www.macmillandictionary.com/media/american/us_pron/c/cha/chair/chair_American_English_pronunciation.mp3');
@@ -25,3 +29,314 @@ const playChairAudio = () => {
 const chairAudio = document.getElementById('chair');
 
 chairAudio.addEventListener('click', playChairAudio)
+
+// lock
+
+const playLockAudio = () => {
+    const lockAudio = new Audio('https://www.macmillandictionary.com/media/american/us_pron/l/loc/lock_/lock_American_English_pronunciation_28450.mp3');
+    lockAudio.play();
+}
+
+const lockAudio = document.getElementById('lock');
+
+lockAudio.addEventListener('click', playLockAudio)
+
+// gum
+
+const playGumAudio = () => {
+    const gumAudio = new Audio('https://www.macmillandictionary.com/media/american/us_pron/g/gum/gum_A/gum_American_English_pronunciation_23071.mp3');
+    gumAudio.play();
+}
+
+const gumAudio = document.getElementById('gum');
+
+gumAudio.addEventListener('click', playGumAudio)
+
+// horn
+
+const playHornAudio = () => {
+    const hornAudio = new Audio('https://www.macmillandictionary.com/media/american/us_pron/h/hor/horn_/horn_American_English_pronunciation_24710.mp3');
+    hornAudio.play();
+}
+
+const hornAudio = document.getElementById('horn');
+
+hornAudio.addEventListener('click', playHornAudio)
+
+// soap 
+
+const playSoapAudio = () => {
+    const soapAudio = new Audio('https://www.macmillandictionary.com/media/american/us_pron/s/soa/soap_/soap_American_English_pronunciation_41799.mp3');
+    soapAudio.play();
+}
+
+const soapAudio = document.getElementById('soap');
+
+soapAudio.addEventListener('click', playSoapAudio)
+
+// map 
+
+const playMapAudio = () => {
+    const mapAudio = new Audio('https://www.macmillandictionary.com/media/american/us_pron/m/map/map_A/map_American_English_pronunciation_29221.mp3');
+    mapAudio.play();
+}
+
+const mapAudio = document.getElementById('map');
+
+mapAudio.addEventListener('click', playMapAudio)
+
+// moose 
+
+const playMooseAudio = () => {
+    const mooseAudio = new Audio('https://www.macmillandictionary.com/media/american/us_pron/m/moo/moose/moose_American_English_pronunciation.mp3');
+    mooseAudio.play();
+}
+
+const mooseAudio = document.getElementById('moose');
+
+mooseAudio.addEventListener('click', playMooseAudio)
+
+
+// // bed
+
+// const playBedAudio = () => {
+//     const bedAudio = new Audio('https://www.macmillandictionary.com/media/american/us_pron/b/bed/bed_A/bed_American_English_pronunciation_15514.mp3');
+//     bedAudio.play();
+// }
+
+// const bedAudio = document.getElementById('bed');
+
+// bedAudio.addEventListener('click', playBedAudio)
+
+// // boat
+
+// const playBoatAudio = () => {
+//     const boatAudio = new Audio('https://www.macmillandictionary.com/media/american/us_pron/b/boa/boat_/boat_American_English_pronunciation.mp3');
+//     boatAudio.play();
+// }
+
+// const boatAudio = document.getElementById('boat');
+
+// boatAudio.addEventListener('click', playBoatAudio)
+
+// // clock
+
+// const playClockAudio = () => {
+//     const clockAudio = new Audio('https://www.macmillandictionary.com/media/american/us_pron/c/clo/clock/clock_American_English_pronunciation_10787.mp3');
+//     clockAudio.play();
+// }
+
+// const clockAudio = document.getElementById('clock');
+
+// clockAudio.addEventListener('click', playClockAudio)
+
+// // crown
+
+// const playCrownAudio = () => {
+//     const crownAudio = new Audio('https://www.macmillandictionary.com/media/american/us_pron/c/cro/crown/crown_American_English_pronunciation_12644.mp3');
+//     crownAudio.play();
+// }
+
+// const crownAudio = document.getElementById('crown');
+
+// crownAudio.addEventListener('click', playCrownAudio)
+
+
+// // duck
+
+// const playDuckAudio = () => {
+//     const duckAudio = new Audio('https://www.macmillandictionary.com/media/american/us_pron/d/duc/duck_/duck_American_English_pronunciation_17854.mp3');
+//     duckAudio.play();
+// }
+
+// const duckAudio = document.getElementById('duck');
+
+// duckAudio.addEventListener('click', playDuckAudio)
+
+// // feet 
+
+// const playFeetAudio = () => {
+//     const feetAudio = new Audio('https://www.macmillandictionary.com/media/american/us_pron/f/fee/feet_/feet_American_English_pronunciation.mp3');
+//     feetAudio.play();
+// }
+
+// const feetAudio = document.getElementById('feet');
+
+// feetAudio.addEventListener('click', playFeetAudio)
+
+// // kite
+
+// const playKiteAudio = () => {
+//     const kiteAudio = new Audio('https://www.macmillandictionary.com/media/american/us_pron/k/kit/kite_/kite_American_English_pronunciation.mp3');
+//     kiteAudio.play();
+// }
+
+// const kiteAudio = document.getElementById('kite');
+
+// kiteAudio.addEventListener('click', playKiteAudio)
+
+// // saw
+
+// const playSawAudio = () => {
+//     const sawAudio = new Audio('https://www.macmillandictionary.com/media/american/us_pron/s/saw/saw_A/saw_American_English_pronunciation_39452.mp3');
+//     sawAudio.play();
+// }
+
+// const sawAudio = document.getElementById('saw');
+
+// sawAudio.addEventListener('click', playSawAudio)
+
+
+// // shell
+
+// const playShellAudio = () => {
+//     const shellAudio = new Audio('https://www.macmillandictionary.com/media/american/us_pron/s/she/shell/shell_American_English_pronunciation_40577.mp3');
+//     shellAudio.play();
+// }
+
+// const shellAudio = document.getElementById('shell');
+
+// shellAudio.addEventListener('click', playShellAudio)
+
+// // bib
+// https://www.macmillandictionary.com/media/american/us_pron/b/bib/bib_A/bib_American_English_pronunciation.mp3
+
+
+// // dock 
+// https://www.macmillandictionary.com/media/american/us_pron/d/doc/dock_/dock_American_English_pronunciation_17224.mp3
+
+
+// // fan
+// https://www.macmillandictionary.com/media/american/us_pron/f/fan/fan_A/fan_American_English_pronunciation_19667.mp3
+
+
+// // gold
+// https://www.macmillandictionary.com/media/american/us_pron/g/gol/gold_/gold_American_English_pronunciation_22424.mp3
+
+
+// // mice
+// https://www.macmillandictionary.com/media/american/us_pron/m/mic/mice_/mice_American_English_pronunciation.mp3
+
+
+// // pig
+// https://www.macmillandictionary.com/media/american/us_pron/p/pig/pig_A/pig_American_English_pronunciation_34672.mp3
+
+
+// // skate
+// https://www.macmillandictionary.com/media/american/us_pron/s/ska/skate/skate_American_English_pronunciation_41222.mp3
+
+
+// // swing
+// https://www.macmillandictionary.com/media/american/us_pron/s/swi/swing/swing_American_English_pronunciation_44116.mp3
+
+
+// // vest
+// https://www.macmillandictionary.com/media/american/us_pron/v/ves/vest_/vest_American_English_pronunciation.mp3
+
+
+// // bird
+// https://www.macmillandictionary.com/media/american/us_pron/b/bir/bird_/bird_American_English_pronunciation.mp3
+
+
+// // bug
+// https://www.macmillandictionary.com/media/american/us_pron/b/bug/bug_A/bug_American_English_pronunciation_15153.mp3
+
+
+// // cone
+// https://www.macmillandictionary.com/media/american/us_pron/c/con/cone_/cone_American_English_pronunciation_11534.mp3
+
+
+// // glass
+// https://www.macmillandictionary.com/media/american/us_pron/g/gla/glass/glass_American_English_pronunciation.mp3
+
+
+// // nail
+// https://www.macmillandictionary.com/media/american/us_pron/n/nai/nail_/nail_American_English_pronunciation.mp3
+
+
+// // pan
+// https://www.macmillandictionary.com/media/american/us_pron/p/pan/pan_A/pan_American_English_pronunciation_33461.mp3
+
+
+// // ring
+// https://www.macmillandictionary.com/media/american/us_pron/r/rin/ring_/ring_American_English_pronunciation.mp3
+
+
+// // sheep
+// https://www.macmillandictionary.com/media/american/us_pron/s/she/sheep/sheep_American_English_pronunciation.mp3
+
+
+// // star
+// https://www.macmillandictionary.com/media/american/us_pron/s/sta/star_/star_American_English_pronunciation_42786.mp3
+
+
+// // clap
+// https://www.macmillandictionary.com/media/american/us_pron/c/cla/clap_/clap_American_English_pronunciation_10631.mp3
+
+
+// // coach
+// https://www.macmillandictionary.com/media/american/us_pron/c/coa/coach/coach_American_English_pronunciation_10901.mp3
+
+
+// // cook
+// https://www.macmillandictionary.com/media/american/us_pron/c/coo/cook_/cook_American_English_pronunciation_11949.mp3
+
+
+// // dish
+// https://www.macmillandictionary.com/media/american/us_pron/d/dis/dish_/dish_American_English_pronunciation_16899.mp3
+
+
+// // grill
+// https://www.macmillandictionary.com/media/american/us_pron/g/gri/grill/grill_American_English_pronunciation.mp3
+
+
+// // pea
+// https://www.macmillandictionary.com/media/american/us_pron/p/pea/pea_A/pea_American_English_pronunciation.mp3
+
+
+// // pool
+// https://www.macmillandictionary.com/media/american/us_pron/p/poo/pool_/pool_American_English_pronunciation_35376.mp3
+
+
+// // skunk
+// https://www.macmillandictionary.com/media/american/us_pron/s/sku/skunk/skunk_American_English_pronunciation.mp3
+
+
+// // tree
+// https://www.macmillandictionary.com/media/american/us_pron/t/tre/tree_/tree_American_English_pronunciation.mp3
+
+
+// // bride
+// https://www.macmillandictionary.com/media/american/us_pron/b/bri/bride/bride_American_English_pronunciation.mp3
+
+
+// // fin
+// https://www.macmillandictionary.com/media/american/us_pron/f/fin/fin_A/fin_American_English_pronunciation.mp3
+
+
+// // frog
+// https://www.macmillandictionary.com/media/american/us_pron/f/fro/frog_/frog_American_English_pronunciation.mp3
+
+
+// // lamp
+// https://www.macmillandictionary.com/media/american/us_pron/l/lam/lamp_/lamp_American_English_pronunciation.mp3
+
+
+// // mail
+// https://www.macmillandictionary.com/media/american/us_pron/m/mai/mail_/mail_American_English_pronunciation_28971.mp3
+
+
+// // rain
+// https://www.macmillandictionary.com/media/american/us_pron/r/rai/rain_/rain_American_English_pronunciation_36947.mp3
+
+
+// // rose
+// https://www.macmillandictionary.com/media/american/us_pron/r/ros/rose_/rose_American_English_pronunciation_38620.mp3
+
+
+// // sink
+// https://www.macmillandictionary.com/media/american/us_pron/s/sin/sink_/sink_American_English_pronunciation_41142.mp3
+
+
+// // top
+// https://www.macmillandictionary.com/media/american/us_pron/t/top/top_A/top_American_English_pronunciation_45588.mp3
